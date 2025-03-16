ddd2fdcd94136359d57ef13c16f7091942dbba60
mona952002 <monasalha22@gmail.com>
2025-03-16 14:53:54 +0200

    first commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="py-4">
+        <a href="{{ url('categories/create') }}" class="btn btn-primary mb-3">أضف صنف جديد</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">اسم الصنف</th>
+                    <th scope="col">الأحداث</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{{$category->name}}</td>
+                        <td>
+                            <a href="{{url('products/delete/'.$category->id)}}" class="btn btn-danger">احذف</a>
+                            <a href="{{url('products/edit/'.$category->id)}}" class="btn btn-info">تعديل</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
